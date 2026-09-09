5ec3049e4c63fd2b07ce2281a73d400775928e46
Ash <88233723+ashwath-ramesh@users.noreply.github.com>
2026-09-09 08:39:23 +0800

    Set Homebrew sha256 values from the v0.1.1 release.

--- Formula/gmail-to-duckdb.rb
+++ Formula/gmail-to-duckdb.rb
@@ -1,23 +1,23 @@
 class GmailToDuckdb < Formula
   desc "Sync Gmail into a local DuckDB file"
   homepage "https://github.com/ashwath-ramesh/gmail-to-duckdb"
-  version "0.1.0"
+  version "0.1.1"
 
   on_macos do
     on_arm do
-      url "https://github.com/ashwath-ramesh/gmail-to-duckdb/releases/download/v0.1.0/gmail-to-duckdb-darwin-arm64.tar.gz"
-      sha256 "f37c34e154682cf1282e5a34803eb9692c69999178c1be109d15459353c8ea79"
+      url "https://github.com/ashwath-ramesh/gmail-to-duckdb/releases/download/v0.1.1/gmail-to-duckdb-darwin-arm64.tar.gz"
+      sha256 "4250ce8a74dc154edfc956e724fdae24db14e0b46ca33a9e2274fe3ca26d5639"
     end
   end
 
   on_linux do
     on_intel do
-      url "https://github.com/ashwath-ramesh/gmail-to-duckdb/releases/download/v0.1.0/gmail-to-duckdb-linux-amd64.tar.gz"
-      sha256 "e7231fd7fc30b5648b90820ffbe9adc1762b03d145e2a40fabcd33cd9c7a5520"
+      url "https://github.com/ashwath-ramesh/gmail-to-duckdb/releases/download/v0.1.1/gmail-to-duckdb-linux-amd64.tar.gz"
+      sha256 "a6dd89d62ba2c448145e19f805f4f7a5d07d28e89d1d9412560bdf16c64cdec6"
     end
     on_arm do
-      url "https://github.com/ashwath-ramesh/gmail-to-duckdb/releases/download/v0.1.0/gmail-to-duckdb-linux-arm64.tar.gz"
-      sha256 "ae8c72453f62e10310ea9abdf6e9a64782208eacb819b26da23cc29b91fbceae"
+      url "https://github.com/ashwath-ramesh/gmail-to-duckdb/releases/download/v0.1.1/gmail-to-duckdb-linux-arm64.tar.gz"
+      sha256 "d691ab54e07e63c52e3b26937c250ff73578a5e547e8420c4325a8df453c4dbd"
     end
   end
 
